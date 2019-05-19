28d55c8454d47831cc39569e35a204bd056ca81e
Adoen Lunnae <unshekdelmonton@gmail.com>
2019-05-19 16:44:00 +0200

    Finished part one

--- Practica-4/funciones.hpp
+++ Practica-4/funciones.hpp
@@ -5,6 +5,7 @@
 #include <cstring>
 #include <cstdlib> //Para usar system
 #include <fstream> //Para trabajar con ficheros
+#include <sstream>
 
 #include "grafo.hpp"
 #include "algoritmosgrafos.hpp"
@@ -14,38 +15,82 @@ using namespace ed;
 
 int menu()
 {
-  int opcion;
+    int opcion;
 
-  system("clear");
-  cout << "  0. Salir................................\n";
-  cout << "  1. Cargar grafo desde fichero...........\n";
-  cout << "  2. Algoritmo de Floyd (GD y GND)......\n";
+    system("clear");
+    cout << "  0. Salir................................\n";
+    cout << "  1. Cargar grafo desde fichero...........\n";
+    cout << "  2. Algoritmo de Floyd (GD y GND)......\n";
 
-  cout << "Introduzca opcion...:";
-  cin >> opcion;
+    cout << "Introduzca opcion...:";
+    cin >> opcion;
 
-  return opcion;
+    return opcion;
 }
 
 template <class G_Nodo, class G_Lado>
-bool cargarGrafo(Grafo<G_Nodo, G_Lado> * &g)
+
+bool cargarGrafo(Grafo<G_Nodo, G_Lado> *g)
 {
-  string ficheroMatriz, ficheroEtiquetas;
+    string ficheroMatriz, ficheroEtiquetas;
+    string tokenLabels, tokenMatrix;
+    int numberRows = 0;
 
-  cout << "Fichero de la matriz de conexion: ";
-  cin >> ficheroMatriz;
+    cout << "Fichero de la matriz de conexion: ";
+    cin >> ficheroMatriz;
 
-  cout << "Fichero de las etiquetas: ";
-  cin >> ficheroEtiquetas;
+    cout << "Fichero de las etiquetas: ";
+    cin >> ficheroEtiquetas;
 
-  // TODO
+    ifstream matrix(ficheroMatriz, ifstream::in);
+    ifstream labels(ficheroEtiquetas, ifstream::in);
+    if (!matrix.is_open())
+    {
+        cout << "Error al abrir el fichero de la matriz\n";
+        return (false);
+    }
+    else if (!labels.is_open())
+    {
+        cout << "Error al abrir el fichero de etiquetas\n";
+        return (false);
+    }
 
+    while (getline(labels, tokenLabels))
+    {
+        numberRows++;
+    }
+    labels.close();
+    labels.open(ficheroEtiquetas);
+    g->reservaMemoria(numberRows);
+
+    for (int i = 0; i < g->getNumNodos(); ++i)
+    {
+        getline(labels, tokenLabels, '\n');
+
+        //Aparece un retorno de carro que hay que eliminar
+        if(*(tokenLabels.end() - 1) == '\r') 
+            tokenLabels.erase(tokenLabels.end() - 1);
+        g->setNodo(i, tokenLabels);
+        for (int j = 0; j < g->getNumNodos() - 1; ++j)
+        {
+            getline(matrix, tokenMatrix, ' ');
+            g->setLado(i, j, atoi(tokenMatrix.c_str()));
+        }
+        getline(matrix, tokenMatrix, '\n');
+        g->setLado(i, g->getNumNodos() - 1, atoi(tokenMatrix.c_str()));
+    }
+
+    matrix.close();
+    labels.close();
+    g->printNodos();
+    g->printLados();
+    return (true);
 }
 
 template <class G_Nodo, class G_Lado>
 void algoritmoFloyd(const Grafo<G_Nodo, G_Lado> &g)
 {
-  // TODO
+    // TODO
 }
 
 #endif
--- Practica-4/grafo.hpp
+++ Practica-4/grafo.hpp
@@ -3,59 +3,128 @@
 
 #include <cassert>
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 namespace ed
 {
 
-	template <class G_Nodo, class G_Lado>
-	class Grafo
-	{
-	public:
+template <class G_Nodo, class G_Lado>
+class Grafo
+{
+public:
+    G_Lado **_lados;
 
-		G_Lado **_lados;
+    G_Nodo *_nodos;
 
-		G_Nodo *_nodos;
+    int _numNodos;
 
-	public:
+public:
+    // constructores
+    Grafo()
+    {
+        _lados = {};
+        _nodos = {};
+        _numNodos = 0;
+    }
 
-		// constructores
-		Grafo(){
-			_lados = {};
-			_nodos = {};
-		}
-		
-		Grafo(int const n){
-			_lados = new G_Lado* [n];
-			for(int i = 0; i < n; i++){
-				_lados[i] = new G_Lado[n];
-			}
-			_nodos = new G_Nodo[n];
-		}
+    Grafo(int const n)
+    {
+        _numNodos = n;
+        _lados = new G_Lado *[n];
+        for (int i = 0; i < n; i++)
+        {
+            _lados[i] = new G_Lado[n];
+        }
+        _nodos = new G_Nodo[n];
+    }
 
-		Grafo(Grafo const g){
-			_nodos = g._nodos;
-			_lados = g._lados;
-		}
+    void reservaMemoria(int n)
+    {
+        borrarGrafo();
+        setNumNodos(n);
+        _lados = new G_Lado *[n];
+        for (int i = 0; i < n; i++)
+        {
+            _lados[i] = new G_Lado[n];
+        }
+        _nodos = new G_Nodo[n];
+    }
 
+    Grafo(const Grafo<G_Nodo, G_Lado> &g)
+    {
+        int n = g.getNumNodos();
+        reservaMemoria(n);
+        for (int i = 0; i < n; i++)
+        {
+            setNodo(i, g.getNodo(i));
+            for (int j = 0; j < n; j++)
+            {
+                setLado(i, j, g.getLado(i, j));
+            }
+        }
+    }
 
+    // destructor
+    ~Grafo()
+    {
+        borrarGrafo();
+        cout << "Destructor usado\n";
+    }
 
-		// destructor
-		~Grafo(){
-			borrarGrafo();		
-		}
+    // funciones
+    // TODO
+    void borrarGrafo()
+    {
+        delete[] _nodos;
+        _nodos = NULL;
+        for (int i = 0; i < _numNodos; i++)
+        {
+            delete[] _lados[i];
+        }
+        delete[] _lados;
+        _lados = NULL;
+        setNumNodos(0);
+    }
 
-		// funciones
-		// TODO
-		void borrarGrafo() {
-			delete[] _lados;
-			_lados = NULL;
-			delete _nodos;
-			_nodos = NULL;
-		}
+    Grafo operator&=(Grafo &g)
+    {
+    }
 
-	};
-}
+    void setLado(int const i, int const j, G_Lado const &l) { _lados[i][j] = l; }
+
+    void setNodo(int const i, G_Nodo const &n) { _nodos[i] = n; }
+
+    void setNumNodos(int const n) { _numNodos = n; }
+
+    G_Lado getLado(int const i, int const j) const { return (_lados[i][j]); }
+
+    G_Nodo getNodo(int const i) const { return (_nodos[i]); }
+
+    int getNumNodos() const { return _numNodos; }
+
+    void printLados() const
+    {
+        for (int i = 0; i < getNumNodos(); i++)
+        {
+            for (int j = 0; j < getNumNodos(); j++)
+            {
+                cout << getLado(i, j) << " ";
+            }
+            cout << "\n\n";
+        }
+    }
+
+    void printNodos() const
+    {
+        for (int i = 0; i < getNumNodos(); i++)
+        {
+            cout << getNodo(i) << " ";
+        }
+        cout << "\n\n";
+    }
+};
+} // namespace ed
 
 #endif
